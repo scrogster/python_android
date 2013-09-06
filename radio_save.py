2303b4c252dac2d2a1bed634b3ba6bc23d3a9610
scrogster <scrogster@gmail.com>
2013-09-06 20:16:58 +1000

    added simple exception handling to radio_save, to allow for possible non-availability of the media stream, due to network problems etc

--- radio_save.py
+++ radio_save.py
@@ -11,22 +11,27 @@ import urllib
 
 #file to save to
 target = open('/mnt/ext_card/music/abc.mp3', "wb")
-#target = open('abc.mp3', "wb")
+try:
+	#RADIO NATIONAL streaming url
+	conn = urllib.urlopen('http://shoutmedia.abc.net.au:10420/')
+	#approx number of minutes desired
+	mins=10
+	#chunk size to write to file
+	chunksize=1024*16
+	#number of chunks to write to get approximately correct recording duration
+	#approximate 630 kB per minute of audio
+	chunks=int(mins*650000/chunksize)
+	#keep buffering and writing the stream intermittently until the required number of seconds has elapsed.
+	#writes in 8kB chunks (could easily be changed depending on CPUspeed/RAM availability).
+	for i in range(1, chunks):
+		target.write(conn.read(chunksize))
+	conn.close()	
+except
+	conn.close()
 
-#RADIO NATIONAL streaming url
-conn = urllib.urlopen('http://shoutmedia.abc.net.au:10420/')
-#approx number of minutes desired
-mins=10
-#chunk size to write to file
-chunksize=1024*8
-#number of chunks to write to get approximately correct recording duration
-#approximate 630 kB per minute of audio
-chunks=int(mins*650000/chunksize)
-#keep buffering and writing the stream intermittently until the required number of seconds has elapsed.
-#writes in 8kB chunks (could be changed depending on CPUspeed/RAM availability).
-for i in range(1, chunks):
-    target.write(conn.read(chunksize))
-conn.close()
+
+
+
 
 
 
