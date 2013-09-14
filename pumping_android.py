67fe5fc319f488b1e01ffbca368cbfdab02ee365
scrogster <scrogster@gmail.com>
2013-09-14 23:19:50 +1000

    Made fixes to allow or new melbourne water website layout. Parsing of html is a bit clunky but works

--- pumping_android.py
+++ pumping_android.py
@@ -7,6 +7,7 @@
 import mechanize
 import HTMLParser
 from BeautifulSoup import BeautifulSoup
+import re
 import android
 app = android.Android()
 
@@ -15,49 +16,53 @@ appMsg = "Retreiving pumping status"
 
 app.dialogCreateSpinnerProgress(appTitle, appMsg)
 app.dialogShow()
-
-BASE_URL = "http://www.melbournewater.com.au/content/rivers_and_creeks/waterway_diverters/yarra_upper.asp"
+            
+BASE_URL = "http://www.melbournewater.com.au/waterdata/waterwaydiversionstatus/Pages/Yarra-River-Upper.aspx"
 br = mechanize.Browser()
 data = br.open(BASE_URL).get_data()
 soup = BeautifulSoup(data)
-table=soup.find("table",title="Table showing current waterway diversion status")
-for row in table.findAll('tr')[1:]:
-    col = row.findAll('td')
-    restrict = col[1]
-    ban = col[2].string
-    flow = col[3].string
-    avflow = col[4].string
-    date = col[5].string
-#Function to strip html tags from table cells (allows for episodic coloring/bolding of cell contents when bans apply!)
-def stripper(data):
-   data = str(data)
-   count = data.count('<')
-   while count:
-       start = data.find('<')
-       end = data.find('>')
-       rem = data[start:end+1]
-       data = data.replace(rem,'',1)
-       count-=1
-   out = data
-   return out
-#apply the stripper function to current restriction and ban statuses, and format strings
-restrict = 'Status?  ' + stripper(restrict)
-ban = 'Banned?  ' + stripper(ban)
+table=soup.find("table",id="ctl00_m_g_a76a034b_68d1_4fca_8b53_c7d6e0cc6fc5_ctl00_grdDivertersSummary" )
+#print(table)
+rows = table.findAll('tr')
+dat=str(rows[1])
+
+def remove_html_tags(data):
+	p = re.compile(r'<.*?>')
+	return p.split(data)
+
+a=remove_html_tags(dat)
+risk=a[3]
+restrict=a[7]
+ban=a[11]
+flow=a[14]
+avflow=a[16]
+dddate=a[18]
+
+##apply the stripper function to current restriction and ban statuses, and format strings
+restrict = 'Restricted?  ' + restrict
+ban = 'Banned?  ' + ban
+risk = 'Risk?    ' + risk
 flow = 'Flow:  ' + flow + ' ML/d'
 avflow = 'Av. Flow:  ' + avflow + ' ML/d'
 
-#output to message box
-#this code borrow extensively from the Linux journal article http://www.linuxjournal.com/article/10940?page=0,2 by Paul Barry
-app.vibrate()
+print(restrict)
+print(ban)
+print(risk)
+print(flow)
+print(avflow)
+
+##output to message box
+##this code borrow extensively from the Linux journal article http://www.linuxjournal.com/article/10940?page=0,2 by Paul Barry
+#app.vibrate()
 
 appMsg = "Pumping status today:"
 app.dialogCreateAlert(appMsg)
-app.dialogSetItems([restrict, ban, flow, avflow])
+app.dialogSetItems([restrict, ban, risk, flow, avflow])
 app.dialogSetPositiveButtonText('OK')
 app.dialogShow()
 resp = app.dialogGetResponse().result
 app.vibrate()
-app.makeToast("Done!")
+#app.makeToast("Done!")
 
 
 
